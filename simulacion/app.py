b2139f0710058775da4bd5b0d1d2524b48284db7
Nestor <nestornav@gmail.com>
2015-12-11 16:20:42 -0300

    Front end. Funciones para generar numeros aleatorios. Primera aproximacion de la primer corrida

--- simulacion/app.py
+++ simulacion/app.py
@@ -5,8 +5,8 @@
 # IMPORTS
 # =============================================================================
 
-from flask import Flask, render_template
-from flask.ext.script import Manager
+from flask import Flask, render_template, request
+from flask.ext.script import Manager, Server 
 from flask_bootstrap import Bootstrap
 
 from . import sim
@@ -17,6 +17,7 @@ from . import sim
 
 app = Flask(__name__)
 cli_manager = Manager(app)
+cli_manager.add_command("runserver", Server(use_reloader=True))
 
 Bootstrap(app)
 
@@ -29,5 +30,13 @@ Bootstrap(app)
 def index():
     return render_template('index.html')
 
+@app.route("/run_simulation",methods=["POST"])
+def run_simulation():
+	media_respuesta = float(request.form["media_respuesta"])
+	desv_respuesta = float(request.form["desv_respuesta"])
+	media_consulta = float(request.form["media_consulta"])
+	resultado = sim.simulate(media_respuesta, desv_respuesta, media_consulta)
+	
+	return "lala"
 
 
--- simulacion/sim.py
+++ simulacion/sim.py
@@ -1,7 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import OrderedDict, namedtuple
+
 import numpy as np
 import scipy
 
 import pygal
+
+ResponseRow = namedtuple(
+	"ResponseRow",
+	["reloj","evento","tiempo_consulta_rnd","tiempo_consulta_total",
+	 "tiempo_respuesta_rnd_db", "tiempo_respuesta_total",
+	 "procesos_activos","db_ocupada","tiempo_ocioso",])
+
+
+def tiempo_respuesta(mean, std):
+	return np.random.normal(mean, std) + 0.1
+
+
+def tiempo_ejecucion_terminal(mean, n):
+	return np.random.exponential(mean, n) + 15
+
+
+def simulate(media_respuesta, desv_respuesta, media_consulta):
+	reloj, procesos_activos, db_ocupada = 0, 0, False
+	eventos = ["Inicio","Fin Ejecucion Consulta","Fin Ejecucion Procesamiento"]
+	terminales = ["ocupada"] * 20
+
+	start_simulation = tiempo_ejecucion_terminal(media_consulta, 20)
+
+	terminales_desordenadas = [(k, v) for k, v in enumerate(start_simulation)]
+	terminales_ordenadas = sorted(terminales_desordenadas, key=lambda e: e[-1]))
+	""" reloj = terminales_ordenadas[0][-1] """
+
+	respuesta = []
+
+	for term_id, start_time terminales_ordenadas:
+		reloj += start_time
+		
+		tiempo_consulta_rnd = start_time
+		tiempo_consulta_total = tiempo_consulta_rnd + reloj
+		tiempo_respuesta_rnd_db = tiempo_respuesta()		
+		tiempo_respuesta_total = tiempo_respuesta_rnd_db + reloj
+		
+		procesos_activos += 1
+		db_ocupada = procesos_activos > 0
+
+		ResponseRow(reloj = reloj, tiempo_consulta_rnd = tiempo_consulta_rnd, tiempo_consulta_total = tiempo_consulta_total,
+					 tiempo_respuesta_rnd_db = tiempo_respuesta_rnd_db, tiempo_respuesta_total = tiempo_respuesta_total,
+					 procesos_activos = procesos_activos, db_ocupada = db_ocupada )
+
+
+
+
+	import ipdb;ipdb.set_trace()
