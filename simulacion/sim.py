6edea8c75850cc57bc3cf0eed1176b18ae518ce6
Nestor <nestornav@gmail.com>
2015-12-11 19:24:01 -0300

    proceso de decision por etapas

--- simulacion/sim.py
+++ simulacion/sim.py
@@ -19,41 +19,96 @@ def tiempo_respuesta(mean, std):
 	return np.random.normal(mean, std) + 0.1
 
 
-def tiempo_ejecucion_terminal(mean, n):
-	return np.random.exponential(mean, n) + 15
+def siguiente_consulta(mean):
+	return np.random.exponential(mean) + 15
 
 
 def simulate(numero_corridas, media_respuesta, desv_respuesta, media_consulta):
-	reloj, procesos_activos, db_ocupada = 0, 0, False
-	eventos = ["Inicio","Fin Ejecucion Consulta","Fin Ejecucion Procesamiento"]
-	terminales = ["ocupada"] * 20
-
-	start_simulation = tiempo_ejecucion_terminal(media_consulta, 20)
-
-	terminales_desordenadas = [(k, v) for k, v in enumerate(start_simulation)]
-	terminales_ordenadas = sorted(terminales_desordenadas, key=lambda e: e[-1])
-	""" reloj = terminales_ordenadas[0][-1] """
-
+	reloj, procesos_activos, db_ocupada = 0., 0, False
+	tiempo_consulta_total, tiempo_respuesta_total = None, None
+	
 	respuestas = []
 	for iterations in xrange(numero_corridas):
-		
-		for term_id, start_time in terminales_ordenadas:
-			reloj += start_time
+		if reloj == 0:
+			tiempo_consulta_rnd = siguiente_consulta()
 			
+			evento = "inicio"			
+			tiempo_consulta_total = tiempo_consulta_rnd + reloj
+			tiempo_consulta_rnd = 0
+			tiempo_respuesta_total = 0
+			procesos_activos = 0
+		elif reloj == tiempo_consulta_total:
+			evento = "Nueva Consulta"
+			tiempo_consulta_rnd = siguiente_consulta()				
+			tiempo_consulta_total = tiempo_consulta_rnd + reloj						
+			
+			procesos_activos = proceso_activos + (1 if proceso_activos < 20 else 0) 
+		elif reloj == tiempo_respuesta_total:
+			evento = "Fin Respuesta a Consulta"
+			tiempo_respuesta_rnd = tiempo_respuesta()
+			tiempo tiempo_respuesta_total = tiempo_respuesta_rnd + reloj
+			
+			
+			procesos_activos = procesos_activos - (1 if procesos_activos > 0 else 0)
+			tiempo_consulta_total =  if procesos_activos < 20 else tiempo_consulta_total
+			
+
+
+		db_ocupada = procesos_activos > 0
+		respuesta = ResponseRow(
+				iteration=iterations, reloj=reloj, evento=evento,
+				tiempo_consulta_rnd=tiempo_consulta_rnd, tiempo_consulta_total=tiempo_consulta_total,
+				tiempo_respuesta_rnd_db=tiempo_respuesta_rnd_db, tiempo_respuesta_total=tiempo_respuesta_total,
+				procesos_activos=procesos_activos, db_ocupada=db_ocupada)
+		respuestas.append(respuesta)
+		
+		reloj = tiempo_consulta_total if tiempo_consulta_total <= tiempo_respuesta_total else tiempo_respuesta_total
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		for term_id, start_time in terminales_ordenadas:		
 			tiempo_consulta_rnd = start_time
 			tiempo_consulta_total = tiempo_consulta_rnd + reloj
-			tiempo_respuesta_rnd_db = tiempo_respuesta()		
-			tiempo_respuesta_total = tiempo_respuesta_rnd_db + reloj
-			
-			procesos_activos += 1
+					
 			db_ocupada = procesos_activos > 0
 
 			respuesta = ResponseRow(
-				iteration=iterations, reloj=reloj, tiempo_consulta_rnd=tiempo_consulta_rnd, tiempo_consulta_total=tiempo_consulta_total,
+				iteration=iterations, reloj=reloj, evento = eventos[0],
+				tiempo_consulta_rnd=tiempo_consulta_rnd, tiempo_consulta_total=tiempo_consulta_total,
 				tiempo_respuesta_rnd_db=tiempo_respuesta_rnd_db, tiempo_respuesta_total=tiempo_respuesta_total,
 				procesos_activos=procesos_activos, db_ocupada=db_ocupada)
 			respuestas.append(respuesta)
 
-
+	
+		#reloj += start_time
+		procesos_activos += 1
+		tiempo_respuesta_rnd_db = tiempo_respuesta()		
+		tiempo_respuesta_total = tiempo_respuesta_rnd_db + reloj
+		
+		
 
 	import ipdb;ipdb.set_trace()
+
+	
