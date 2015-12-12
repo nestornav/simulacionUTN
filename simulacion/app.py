e4cf424ba22b81c1236f996b417fdf9ba1dd2d46
Juan B Cabral <jbc.develop@gmail.com>
2015-12-12 00:03:39 -0300

    fixes

--- simulacion/app.py
+++ simulacion/app.py
@@ -6,7 +6,7 @@
 # =============================================================================
 
 from flask import Flask, render_template, request
-from flask.ext.script import Manager, Server 
+from flask.ext.script import Manager, Server
 from flask_bootstrap import Bootstrap
 
 from . import sim
@@ -34,25 +34,25 @@ dnumero_corridas = 100
 @app.route("/")
 def index():
     return render_template(
-    	'index.html', media_respuesta=dmedia_respuesta, 
-		desv_respuesta=ddesv_respuesta, media_consulta=dmedia_consulta,
-    	numero_corridas=dnumero_corridas)
+        'index.html', media_respuesta=dmedia_respuesta,
+        desv_respuesta=ddesv_respuesta, media_consulta=dmedia_consulta,
+        numero_corridas=dnumero_corridas)
 
 
 @app.route("/run_simulation",methods=["POST"])
 def run_simulation():
-	media_respuesta = float(request.form["media_respuesta"])
-	desv_respuesta = float(request.form["desv_respuesta"])
-	media_consulta = float(request.form["media_consulta"])
-	numero_corridas = int(request.form["numero_corridas"])
-	
-	resultados = sim.simulate(numero_corridas, media_respuesta, desv_respuesta, media_consulta)
-	
-	return render_template('index.html', resultados=resultados, media_respuesta=media_respuesta, 
-		desv_respuesta=desv_respuesta, media_consulta=media_consulta,
-    	numero_corridas=numero_corridas)
+    media_respuesta = float(request.form["media_respuesta"])
+    desv_respuesta = float(request.form["desv_respuesta"])
+    media_consulta = float(request.form["media_consulta"])
+    numero_corridas = int(request.form["numero_corridas"])
 
-	
+    resultados = sim.simulate(numero_corridas, media_respuesta, desv_respuesta, media_consulta)
+
+    return render_template('index.html', resultados=resultados, media_respuesta=media_respuesta,
+        desv_respuesta=desv_respuesta, media_consulta=media_consulta,
+        numero_corridas=numero_corridas)
+
+
 
 
 
--- simulacion/sim.py
+++ simulacion/sim.py
@@ -9,73 +9,72 @@ import scipy
 import pygal
 
 ResponseRow = namedtuple(
-	"ResponseRow",
-	["iteration", "reloj","evento","tiempo_consulta_rnd","tiempo_consulta_total",
-	 "tiempo_respuesta_rnd_db", "tiempo_respuesta_total",
-	 "procesos_activos","db_ocupada","tiempo_ocioso"])
+    "ResponseRow",
+    ["iteration", "reloj","evento","tiempo_consulta_rnd","tiempo_consulta_total",
+     "tiempo_respuesta_rnd_db", "tiempo_respuesta_total",
+     "procesos_activos","db_ocupada","tiempo_ocioso"])
 
 
 def tiempo_respuesta(mean, std):
-	return np.random.normal(mean, std) + 0.1
+    return np.random.normal(mean, std) + 0.1
 
 
 def siguiente_consulta(mean):
-	beta = 1/mean
-	return np.random.exponential(beta) + 15
+    beta = 1/mean
+    return np.random.exponential(beta) + 15
 
 
 def simulate(numero_corridas, media_respuesta, desv_respuesta, media_consulta):
-	reloj, db_ocupada, procesos_activos = 0., False, 0
-	tiempo_consulta_total, tiempo_respuesta_total = None, None
-	
-	respuestas = []	
-	#import ipdb;ipdb.set_trace()
-	for iterations in xrange(numero_corridas):
-		if reloj == 0:
-			tiempo_consulta_rnd = siguiente_consulta(media_consulta)
-			
-			evento = "inicio"			
-			tiempo_consulta_total = tiempo_consulta_rnd + reloj			
-			tiempo_respuesta_rnd_db = 0
-			tiempo_respuesta_total = 0				
+    reloj, db_ocupada, procesos_activos = 0., False, 0
+    tiempo_consulta_total, tiempo_respuesta_total = None, None
 
-		elif reloj == tiempo_consulta_total:
-			evento = "Nueva Consulta"
-			tiempo_consulta_rnd = siguiente_consulta(media_consulta)				
-			tiempo_consulta_total = tiempo_consulta_rnd + reloj						
+    respuestas = []
+    #import ipdb;ipdb.set_trace()
+    for iterations in xrange(numero_corridas):
+        if reloj == 0:
+            tiempo_consulta_rnd = siguiente_consulta(media_consulta)
 
-			tiempo_respuesta_rnd_db = tiempo_respuesta(media_respuesta, desv_respuesta)
-			tiempo_respuesta_total = tiempo_respuesta_rnd_db + reloj			
-			procesos_activos += 1
-						
-		elif reloj == tiempo_respuesta_total:
-			evento = "Fin Respuesta a Consulta"			
-			procesos_activos -= 1
-			
-			if procesos_activos > 0:				
-				tiempo_respuesta_rnd_db = tiempo_respuesta(media_respuesta, desv_respuesta)
-				tiempo_respuesta_total = tiempo_respuesta_rnd_db + reloj
-			else:
-				tiempo_respuesta_rnd_db = 0
-				tiempo_respuesta_total = 0
+            evento = "inicio"
+            tiempo_consulta_total = tiempo_consulta_rnd + reloj
+            tiempo_respuesta_rnd_db = 0
+            tiempo_respuesta_total = 0
 
-		
-		db_ocupada = procesos_activos > 0
+        elif reloj == tiempo_consulta_total:
+            evento = "Nueva Consulta"
+            tiempo_consulta_rnd = siguiente_consulta(media_consulta)
+            tiempo_consulta_total = tiempo_consulta_rnd + reloj
 
-		respuesta = ResponseRow(
-				iteration=iterations, reloj=reloj, evento=evento,
-				tiempo_consulta_rnd=tiempo_consulta_rnd, tiempo_consulta_total=tiempo_consulta_total,
-				tiempo_respuesta_rnd_db=tiempo_respuesta_rnd_db, tiempo_respuesta_total=tiempo_respuesta_total,
-				procesos_activos=procesos_activos, db_ocupada=db_ocupada, tiempo_ocioso=0)
-		respuestas.append(respuesta)
-				
-		if tiempo_respuesta_total >0:
-			if tiempo_consulta_total <= tiempo_respuesta_total:
-				reloj = tiempo_consulta_total
-			else :
-				reloj = tiempo_respuesta_total
-		else:
-			reloj = tiempo_consulta_total
+            tiempo_respuesta_rnd_db = tiempo_respuesta(media_respuesta, desv_respuesta)
+            tiempo_respuesta_total = tiempo_respuesta_rnd_db + reloj
+            procesos_activos += 1
 
-	return respuestas
-	
+        elif reloj == tiempo_respuesta_total:
+            evento = "Fin Respuesta"
+            procesos_activos -= 1
+
+            if procesos_activos > 0:
+                tiempo_respuesta_rnd_db = tiempo_respuesta(media_respuesta, desv_respuesta)
+                tiempo_respuesta_total = tiempo_respuesta_rnd_db + reloj
+            else:
+                tiempo_respuesta_rnd_db = 0
+                tiempo_respuesta_total = 0
+
+
+        db_ocupada = procesos_activos > 0
+
+        respuesta = ResponseRow(
+                iteration=iterations, reloj=reloj, evento=evento,
+                tiempo_consulta_rnd=tiempo_consulta_rnd, tiempo_consulta_total=tiempo_consulta_total,
+                tiempo_respuesta_rnd_db=tiempo_respuesta_rnd_db, tiempo_respuesta_total=tiempo_respuesta_total,
+                procesos_activos=procesos_activos, db_ocupada=db_ocupada, tiempo_ocioso=0)
+        respuestas.append(respuesta)
+
+        if tiempo_respuesta_total >0:
+            if tiempo_consulta_total <= tiempo_respuesta_total:
+                reloj = tiempo_consulta_total
+            else :
+                reloj = tiempo_respuesta_total
+        else:
+            reloj = tiempo_consulta_total
+
+    return respuestas
